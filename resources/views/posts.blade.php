92c817a1e4529003026a8d1cb71589ad939fde0b
Parker Parham <parkerparham@parkers-mbp.lan>
2023-09-08 16:16:58 -0400

    changes

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,29 +7,12 @@
     <title>My Blog</title>
 </head>
 <body>
-    <article>
-        <h1><a href="/post">My First Post</a></h1>
-
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia blanditiis dolores ex nisi, dicta quasi reprehenderit, iure laboriosam maiores ipsum earum itaque aliquid nostrum? Praesentium quia dolore enim accusantium, amet ea facere necessitatibus iste omnis inventore non saepe illum expedita doloribus similique blanditiis ad quam nesciunt, sit fugit harum maiores.
-        </p>
-    </article>
+    <?php foreach ($posts as $post) : ?>
 
     <article>
-        <h1><a href="/post">My Second Post</a></h1>
-
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia blanditiis dolores ex nisi, dicta quasi reprehenderit, iure laboriosam maiores ipsum earum itaque aliquid nostrum? Praesentium quia dolore enim accusantium, amet ea facere necessitatibus iste omnis inventore non saepe illum expedita doloribus similique blanditiis ad quam nesciunt, sit fugit harum maiores.
-        </p>
+        <?= $post; ?>
     </article>
-
-    <article>
-        <h1><a href="/post">My Third Post</a></h1>
-
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia blanditiis dolores ex nisi, dicta quasi reprehenderit, iure laboriosam maiores ipsum earum itaque aliquid nostrum? Praesentium quia dolore enim accusantium, amet ea facere necessitatibus iste omnis inventore non saepe illum expedita doloribus similique blanditiis ad quam nesciunt, sit fugit harum maiores.
-        </p>
-    </article>
+    <?php endforeach; ?>
     
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,11 +15,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('posts');
-});
 
-Route::get('post', function() {
-    return view('post', [
-        'post' => file_get_contents(__DIR__ . '/../resources/posts/my-first-post.html')
+    return view('posts', [
+        'posts' => Post::all()
     ]);
 });
+
+Route::get('posts/{post}', function($slug) {
+    return view('post', [
+        'post' => Post::find($slug)
+    ]);
+
+})->where('post', '[A-z_\-]+');
