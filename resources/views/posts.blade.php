069500bb7c2e81a60b112752f01e4982a8220f50
Parker Parham <parkerparham@parkers-mbp.lan>
2023-09-08 14:55:56 -0400

    Initial commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/app.css">
+    <title>My Blog</title>
+</head>
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia blanditiis dolores ex nisi, dicta quasi reprehenderit, iure laboriosam maiores ipsum earum itaque aliquid nostrum? Praesentium quia dolore enim accusantium, amet ea facere necessitatibus iste omnis inventore non saepe illum expedita doloribus similique blanditiis ad quam nesciunt, sit fugit harum maiores.
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia blanditiis dolores ex nisi, dicta quasi reprehenderit, iure laboriosam maiores ipsum earum itaque aliquid nostrum? Praesentium quia dolore enim accusantium, amet ea facere necessitatibus iste omnis inventore non saepe illum expedita doloribus similique blanditiis ad quam nesciunt, sit fugit harum maiores.
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Third Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia blanditiis dolores ex nisi, dicta quasi reprehenderit, iure laboriosam maiores ipsum earum itaque aliquid nostrum? Praesentium quia dolore enim accusantium, amet ea facere necessitatibus iste omnis inventore non saepe illum expedita doloribus similique blanditiis ad quam nesciunt, sit fugit harum maiores.
+        </p>
+    </article>
+    
+</body>
+</html>
