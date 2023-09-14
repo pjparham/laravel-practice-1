7c46d22fa4eae4d4ae56b840e085c1e18765b00e
Parker Parham <parkerparham@parkers-mbp.lan>
2023-09-14 11:45:29 -0400

    Updates post methods

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 // use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 
 class Post
@@ -25,20 +26,19 @@ class Post
     }
 
     public static function all(){
-
-        $files = File::files(resource_path("posts/"));
-
-        return array_map(fn($file) => $file->getContents(), $files);
+        return collect(File::files(resource_path("posts")))
+        ->map(fn($file) => YamlFrontMatter::parseFile($file))
+        ->map(fn($document) => new Post(
+                $document->title,
+                $document->excerpt,
+                $document->date,
+                $document->body(),
+                $document->slug,
+        ));
     }
 
     public static function find($slug){
-        if (!file_exists($path = resource_path("posts/{$slug}.html"))){
-            throw new ModelNotFoundException();
-        }
-
-        $post = cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
-
-        return $post;
+        return static::all()->firstWhere('slug', $slug);
     }
     use HasFactory;
 }
