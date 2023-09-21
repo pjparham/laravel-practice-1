91c2d6ba206c86d03c1591d04dbe705f055b84ad
Parker Parham <parkerparham@gmail.com>
2023-09-21 11:04:57 -0400

    adds fillable values to post class, updates page rendering

--- routes/web.php
+++ routes/web.php
@@ -20,10 +20,9 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('posts/{post}', function($id) {
+Route::get('posts/{post:slug}', function(Post $post) {
     return view('post', [
-        // comment
-        'post' => Post::find($id)
+        'post' => $post
     ]);
 
 });
