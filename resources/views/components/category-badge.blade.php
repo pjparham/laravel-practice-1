147142c0ef1f0d8f9449a5f19755143c743e87cc
Parker Parham <parkerparham@gmail.com>
2023-09-22 16:30:00 -0400

    makes dynamic post page

---
+++ resources/views/components/category-badge.blade.php
@@ -0,0 +1,6 @@
+@props(['post'])
+<a href="/categories/{{$post->category->slug}}" 
+        class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold" 
+        >
+    {{ $post->category->name}}
+</a>
