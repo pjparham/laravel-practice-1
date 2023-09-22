e80cd3e418cefa981bd20f1e6fece1de57b9eb34
Parker Parham <parkerparham@gmail.com>
2023-09-22 09:35:30 -0400

    adds user model association access

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,57 +22,25 @@ class DatabaseSeeder extends Seeder
         Post::truncate();
 
         $user = User::factory()->create();
-        
-        $travel = Category::create([
+
+        $cat1 = Category::factory()->create([
             'name' => 'Travel',
             'slug' => 'travel'
         ]);
-        
-        $tech = Category::create([
-            'name' => 'Tech',
-            'slug' => 'tech'
-        ]);
 
-        $food= Category::create([
+        $cat2 = Category::factory()->create([
             'name' => 'Food',
             'slug' => 'food'
         ]);
 
-        Post::create([
+        Post::factory(3)->create([
             'user_id' => $user->id,
-            'category_id' => $travel->id,
-            'title' => 'The Journey Begins',
-            'excerpt' => 'Embarking on a new adventure...',
-            'slug' => Str::slug('The Journey Begins'),
-            'body' => 'Today marks the start of a thrilling journey filled with excitement and uncertainty. As I step into the unknown, I am reminded of the countless possibilities that lie ahead. With a backpack on my shoulders and a heart full of dreams, I\'m ready to explore the world and create memories that will last a lifetime.'
+            'category_id' => $cat1->id,
         ]);
 
-
-        Post::create([
+        Post::factory(2)->create([
             'user_id' => $user->id,
-            'category_id' => $travel->id,
-            'title' => 'Exploring Nature',
-            'excerpt' => 'A day in the wilderness...',
-            'slug' => Str::slug('Exploring Nature'),
-            'body' => 'There\'s something magical about spending a day in the heart of nature. The rustling of leaves in the breeze, the gentle babble of a nearby stream, and the chirping of birds create a symphony of serenity. As I hike through the lush forests and gaze upon breathtaking vistas, I am reminded of the beauty and wonder that surround us.'
-        ]);
- 
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $tech->id,
-            'title' => 'Tech Trends 2023',
-            'excerpt' => 'The latest in technology innovations...',
-            'slug' => Str::slug('Tech Trends 2023'),
-            'body' => 'In the ever-evolving world of technology, 2023 promises to be a groundbreaking year. From the rise of artificial intelligence and augmented reality to the development of sustainable energy solutions, the tech landscape is on the verge of transformation. Join me as we explore the trends and innovations that will shape our future.'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $food->id,
-            'title' => 'Delicious Recipes',
-            'excerpt' => 'Cooking up culinary delights...',
-            'slug' => Str::slug('Delicious Recipes'),
-            'body' => 'There\'s nothing quite like the joy of creating delicious meals in the kitchen. From aromatic spices to fresh ingredients, cooking is an art that brings people together. In this blog, I will share some of my favorite recipes and culinary adventures. Get ready to embark on a gastronomic journey filled with flavors, techniques, and culinary inspiration.'
+            'category_id' => $cat2->id,
         ]);
 
 
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 /*
@@ -17,7 +18,7 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 Route::get('/', function () {
     return view('posts', [
-        'posts' => Post::with('category')->get()
+        'posts' => Post::latest()->with('category', 'author')->get()
     ]);
 });
 
@@ -29,6 +30,12 @@ Route::get('posts/{post:slug}', function(Post $post) {
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
-        'posts' => $category->posts
+        'posts' => $category->posts->load(['category', 'author'])
+    ]);
+});
+
+Route::get('authors/{author:username}', function (User $author) {
+    return view('posts', [
+        'posts' => $author->posts->load(['category', 'author'])
     ]);
 });
