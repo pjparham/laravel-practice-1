a261d80d87d2907edde03fcc2cb1ec6dc62bb8a5
Parker Parham <parkerparham@gmail.com>
2023-10-03 11:31:56 -0400

    adds post controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,64 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $posts = Post::latest()->with('category', 'author');
+
+        if (request('search')) {
+            $posts
+                ->where('title', 'like', '%' . request('search') . '%')
+                ->orWhere('body', 'like', '%' . request('search') . '%');
+        }
+
+        return view('posts', [
+            'posts' => $posts->get(),
+            'categories' => Category::all()
+        ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
     public function show(Post $post)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Post $post)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Post $post)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Post $post)
-    {
-        //
+        return view('post', [
+            'post' => $post
+        ]);
     }
 }
--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -6,7 +6,7 @@
         {{ $trigger }}
     </div>
 
-    <div x-show="show" class="py-2 absolute bg-gray-100 w-32 mt-2 rounded-xl z-40" style="display: none">
+    <div x-show="show" class="py-2 absolute bg-gray-100 w-32 mt-2 rounded-xl z-40 overflow-auto max-h-52" style="display: none">
         {{ $slot  }}
     </div>
 </div>
